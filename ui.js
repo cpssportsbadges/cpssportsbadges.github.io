c097ce198529c4a4c218f09b653b3540acd04b2d
Joseph G. Varilla <jvarilla981@gmail.com>
2018-09-13 21:20:06 -0500

    almost there

--- ui.js
+++ ui.js
@@ -4,11 +4,31 @@ $(() => {
 
 
 	//hover horizontal cards
-	$(".horizontalCard").click(() => {
+	$(".horizontalCard").click(function() {
 		$(this).hide();
 		console.log(this);
 	})
-	verticalCards.hide();
+
+	$(".horizontalCard").mouseover(function() {
+		$(this).children().show();
+		console.log(this);
+	})
+	$(".horizontalCard").mouseout(function() {
+		$(this).children()
+		$(this).children('.horizontalCardBody').hide();
+		console.log(this);
+	})
+
+	$("#skills, #projects").mouseenter(function() {
+		$(this).children().children('.verticalCardBody').show();
+		console.log(this);
+	})
+
+	$("#skills, #projects").mouseleave(function() {
+		$(this).children().children('.verticalCardBody').hide();
+		console.log(this);
+	})
+
 	$('body')
 
 })
