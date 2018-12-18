163cea396293d3e574bbbf7c95eeb75fa6cf50c4
Joseph G. Varilla <jvarilla981@gmail.com>
2018-12-17 23:45:29 -0600

    Project cards expand on click

--- public/ui.js
+++ public/ui.js
@@ -58,6 +58,93 @@ AOS.init({
 			child.classList.contains("project-caption-container");
 		});
 	}
+
+	let focusedCard = null;
+	function focusOnProjectSlide(event, elemId) {
+		const targetCard = document.getElementById(elemId);
+		if (targetCard !== focusedCard) {
+			if (focusedCard) {
+				focusedCard.classList.remove('projectCardFocused');
+			}
+			focusedCard = targetCard;
+			loadProjectData(elemId);
+		} else {
+			hideProjectData();
+			focusedCard = null;
+		}
+		console.log(event.target);
+		
+		targetCard.classList.toggle('projectCardFocused');
+		console.log('fired');
+	}
+
+	const projectCardDataSet = {
+		"mcyaf-project-card": {
+			title: "MCYAF Site Renovation",
+			description: 
+				"Volunteer with the Maine Community Youth Assistance Foundation to create a new content management system website and to train them on data entry and maintenance.",
+			date: "2017",
+			status: "Live- Standby Maintenance",
+			link: "https://www.mcyaf.com",
+			stack: ["HTML", "CSS"],
+			skills: ["Consulting", "Content Managment Systems", "User Training", "Web Design"],
+		},
+		"facedetection-project-card": {
+			title: "Facial Detection App",
+			description: "A full-stack web application that makes use of Clarifai's facial detection API deployed onto Heroku.",
+			date: "2018",
+			status: "Live",
+			link: "",
+			stack: ["JavaScript", "HTML", "CSS", "Node.js", "ReactJS", "PostgreSQL"],
+			skills: ["Full-stack Development", "Deployment"],
+		},
+		"owe-knowe-project-card": {
+			title: "Owe Knowe",
+			description: "A web application that allows you to keep track of how much you owe and how much you are owed, complete with interest calculations.",
+			date: "2018",
+			status: "In Development",
+			link: "",
+			stack: ["jQuery", "jQuery UI", "JavaScript", "HTML", "CSS"],
+			skills: [],
+		},
+		"advanced-minecraft-project-card": {
+			title: "Advanced Minecraft Mods",
+			description: "Minecraft Mods made to work on CodeKingdom's platform for Minecraft, used to teach object-oriented programming to K-12 students.",
+			date: "2018",
+			status: "Ongoing",
+			link: "https://www.github.com/jvarilla/minecraftAdvanced",
+			stack: ["java"],
+			skills: ["teaching", "OOP"],
+		},
+		"coming-soon-project-card": {
+			title: "Coming Soon",
+			description: "???",
+			date: "Future",
+			status: "",
+			link: "",
+			stack: [],
+			skills: [],
+		},
+	}
+
+	const projectDataCard = document.getElementById("dynamic-project-data-card");
+	function loadProjectData(elemId) {
+		const dataSet = projectCardDataSet[elemId];
+		projectDataCard.classList.add("projectDataCardActive");
+		document.getElementById("dynamic-project-title").appendChild(document.createTextNode(dataSet.title));
+	}
+
+	function hideProjectData() {
+		projectDataCard.classList.remove("projectDataCardActive");
+	}
+
+	// $(() => {
+	// 	$(".projectCard").on('click', () => {
+	// 		console.log(this);
+	// 		this.classList.add('projectCardFocused');
+	// 	})
+	// })
+
 	// function revealCaption2() {
 	// 	$(this).children(".project-caption-container").css('display', 'block');
 	// 	// console.log(captionBlock[0]);
