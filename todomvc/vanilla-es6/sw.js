b8288a78f05acf7fb5958d10cfcff9f918da2be4
Joseph Varilla <jvarilla981@gmail.com>
2019-02-23 22:17:09 -0600

    fix sw.js

--- todomvc/vanilla-es6/sw.js
+++ todomvc/vanilla-es6/sw.js
@@ -1,15 +1,3 @@
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', function() {
-		navigator.serviceWorker.register('/sw.js').then((registration) {
-			//Registration was successful
-			console.log('ServiceWorker registaion succeeded')
-		}, function(err) {
-			//registration failed :(
-			console.log('ServiceWorker regisration failed')
-		});
-	});
-}
-
 let CACHE_NAME = 'vanilla-es6';
 let urlsToCache = [
 	'index.html',
