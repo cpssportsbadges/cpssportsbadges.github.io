255e3207a603f68ef17d46fe9e2c803602b41444
Joseph Varilla <jvarilla981@gmail.com>
2019-02-23 20:50:31 -0600

    modify sw

--- todomvc/vanilla-es6/sw.js
+++ todomvc/vanilla-es6/sw.js
@@ -1,1 +1,32 @@
-if 
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', function() {
+		navigator.serviceWorker.register('/sw.js').then((registration) {
+			//Registration was successful
+			console.log('ServiceWorker registaion succeeded')
+		}, function(err) {
+			//registration failed :(
+			console.log('ServiceWorker regisration failed')
+		});
+	});
+}
+
+let CACHE_NAME = 'vanilla-es6';
+let urlsToCache = [
+	'/index.html',
+	'/dist/bundle.js',
+	'/node_modules/todomvc-common/base.js',
+	'/node_modules/todomvc-common/base.css',
+	'/node_modules/todomvc-app-css/index.css'
+];
+
+self.addEventListener('install', function(event) {
+	// Perform install steps
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(function(cache) {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			}) 
+	);
+});
+
