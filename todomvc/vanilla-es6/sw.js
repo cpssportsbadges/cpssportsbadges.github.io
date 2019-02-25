1e7fc153797fdbfe5acaf0288e586066e90edb94
Joseph Varilla <jvarilla981@gmail.com>
2019-02-24 18:04:42 -0600

    add fetch request handler to sw.js

--- todomvc/vanilla-es6/sw.js
+++ todomvc/vanilla-es6/sw.js
@@ -23,3 +23,16 @@ self.addEventListener('install', function(event) {
 	);
 });
 
+
+// when the browser fetches a url, either response with
+// the cached object or go ahead and fetch the actual url
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // ensure we check the *right* cache to match against
+    caches.open(cacheName).then(cache => {
+      return cache.match(event.request).then(res => {
+        return res || fetch(event.request)
+      });
+    })
+  );
+});
