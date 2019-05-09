bb50464e20a538c4c1be62885240404c671f2958
Joseph Varilla <jvarilla981@gmail.com>
2019-05-09 17:25:47 -0500

    Locked rows for excel

--- BadgeRenderer/badgeLayersConfig.js
+++ BadgeRenderer/badgeLayersConfig.js
@@ -5,10 +5,18 @@ const badgeLayersConfig = {//paths relative to BadgeRenderer
 		// purple: './BadgeAssets/badge-background/badge-base-hexagon-purple.png',
 		// red: './BadgeAssets/badge-background/badge-base-hexagon-red.png'
 		distinction: {
+			// a: "./BadgeV3/oldbase_blank_hole.png",
+			// 0: "./BadgeV3/oldbase_blank_hole.png",
 			a: "./BadgeV3/BaseLayers/defaultBaseLayer.png",
 			0: "./BadgeV3/BaseLayers/defaultBaseLayer.png",
 			1: "./BadgeV3/BaseLayers/goldBaseLayer.png",
-			2: "./BadgeV3/BaseLayers/silverBaseLayer.png"
+			2: "./BadgeV3/BaseLayers/silverBaseLayer.png",
+			v4: {
+				a: "./BadgeV4/Base-Layers/CPS_Sports_Base_Layer-Default.png",
+				0: "./BadgeV4/Base-Layers/CPS_Sports_Base_Layer-Default.png",
+				1: "./BadgeV4/Base-Layers/CPS_Sports_Base_Layer-Gold.png",
+				2: "./BadgeV4/Base-Layers/CPS_Sports_Base_Layer-Silver.png",
+			}
 		}
 	},
 	overlay: {
