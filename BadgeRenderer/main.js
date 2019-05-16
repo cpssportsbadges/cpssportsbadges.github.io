a8d68c78b91737bbf94aed64a0bbe5e2bb77f8eb
Joseph Varilla <jvarilla981@gmail.com>
2019-05-16 17:54:04 -0500

    Document rederer code and update package.json with dev dependencies

--- BadgeRenderer/main.js
+++ BadgeRenderer/main.js
@@ -1,13 +1,27 @@
+/* 
+  main.js
+  About: 
+  Powers the user interface
+  of the BadgeRenderer. This file makes
+  use of jQuery and some NPM packages.
+
+  main.js does not actually get sent to
+  the user's browser, you need to build all
+  the dependencies into bundle.js which get sent over.
+
+  Created by: Joseph Varilla
+  Last Edited: 5/16/2019
+*/
 let CryptoJS = require("crypto-js");
 let BadgeImageConfig = require("./badgeLayersConfig.js");
 const RenderEngine = require("./RenderEngine.js");
-const RenderEngineV2 = require("./RenderEngineV2.js");
+//const RenderEngineV2 = require("./RenderEngineV2.js");
 
 let jsPDF = require("jspdf");
-console.log(BadgeImageConfig);
 let decryptedText;
 let ciphertext;
 
+/* Creates the badge html that is embeddable */
 function generateBadgeHTML(badgeCanvasId) {
 		return `<div><img src="${document.getElementById(badgeCanvasId).toDataURL('image/png', 1.0)}"/></div>`;
 }	
@@ -22,6 +36,8 @@ $(() => {
 	function parseQueryString(name) { //gets query string parameters
 	    const url = window.location.href;
 	    if (!url) url = window.location.href;
+	    // Remove fragment part of the url
+	    // url = url.split("#")[0];
 	    name = name.replace(/[\[\]]/g, "\\$&");
 	    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
 	        results = regex.exec(url);
@@ -29,7 +45,6 @@ $(() => {
 	    if (!results[2]) return '';
 	    let returnParam = decodeURIComponent(decodeURIComponent(results[2].replace(/\+/g, " ")));
 	    returnParam = returnParam.replace(/ /g, '+');
-	    console.log(returnParam);
 	    return returnParam;
 	}
 
@@ -40,7 +55,8 @@ $(() => {
 		
 	}
 
-	function getBadgesFromLocalStorage() { // Gets the Badge Data From Local Storage
+	// Gets the Badge Data From Local Storage
+	function getBadgesFromLocalStorage() { 
 		let badges = [];
 		for (element in window.localStorage) {
 			if (element.includes("Badge")) {
@@ -50,8 +66,9 @@ $(() => {
 		return badges;
 	}
 
-	async function renderMultipleBadges() { // Renders the Badge Cards of already unlocked badges
-		console.log("rendered multiple badges");
+	// Renders the Badge Cards of already unlocked badges
+	// Sorts by recency of award new badges to the left
+	async function renderMultipleBadges() { 
 		let badges = await getBadgesFromLocalStorage();
 		badges.sort(function(a, b) {
 			let aDate = new Date(a.awardDate);
@@ -62,7 +79,6 @@ $(() => {
 			// Add To The MapofBadges
 			let badgeMapKey = "Badge-" + index;
 			mapOfBadges[badgeMapKey] = badge;
-			console.log(mapOfBadges);
 			let canvasId = `Badge-${index}`;
 			let badgeCardId = "badgeCard" + index
 
@@ -104,13 +120,9 @@ $(() => {
 		});
 	}
 
-
-	
+	// Call to render the mutiple badges
 	renderMultipleBadges();
 
-
-
-	
 
 	// Hide and show the unlock badge field as necessary
 	if(parseQueryString('ebs')) {
@@ -120,15 +132,16 @@ $(() => {
 	} else {
 		$("#needsToBeUnlocked").hide();
 	}
-	//Store json value in browser using websql
 
-
+	// Handles if a badge is unlocked
 	$("#unlockBadgeBtn").on('click', function(event) {
+		// Get Key to use to unlock the badge
 		let key = $("#keyInput").val();
 		let feedback = $("#feedback");
 		let hash = parseQueryString('ebs');
-		//console.log(localStorage[hash]);
-		//let badgeData = decryptQueryString(hash, key);
+
+		/* If the badge does not yet exist in local storage
+		   If you don't have the badge already */
 		if (!window.localStorage['Badge' + hash]) {
 			try {
 				let badgeData = decryptQueryString(hash, key);
@@ -144,114 +157,45 @@ $(() => {
 
 				badgeData.badgeName = badgeData.awardName;
 				
-				//renderBadge(badgeData);
-				console.log(badgeData);
-				// $("#badgeImage").attr('src', drawBadge(decryptQueryString(hash, key)));
-				//console.log(drawBadge(decryptQueryString(hash, key)));
+				// Create canvas to draw the badge
 				let canvasId = `Badge-${key}`;
 				$("#badges").append(`<canvas id=${canvasId} height="800" width="1000" hidden></canvas>`);
-				// drawBadge(badgeData, canvasId);
 				let canvas = document.getElementById(canvasId);
-				let oldBadgeConfiguration = [
-					{
-						type: "image",
-						name: "background",
-						imagePath: BadgeImageConfig['background'][badgeData['conference']],
-						x: 0,
-						y: 0
-					},
-					{
-						type: "image",
-						name: "sport",
-						imagePath: BadgeImageConfig['sport'][badgeData['sport']],
-						x: 0,
-						y: 0
-					},
-					{
-						type: "image", 
-						name: "overlay",
-						imagePath: BadgeImageConfig['overlay'][badgeData['overlay']],
-						x: 0,
-						y: 0
-					},
-					{
-						type: "text",
-						name: "badge name",
-						fontSize: "36px",
-						fontFamily: "Helvetica",
-						fontColor: "#ffffff",
-						x: 400,
-						y: 400,
-						text: badgeData.badgeName
-					},
-					{
-						type: "text",
-						name: "recpient name",
-						fontSize: "20px",
-						fontFamily: "Helvetica",
-						x: 400,
-						y: 475,
-						text: badgeData.recipientName
-					}
-				]
+				
+				// Get the correct badge image layers based on badge data
 
+				// Get the correct badge background based on distinction
 				let backgroundImgPath = BadgeImageConfig['background']['distinction']['v4'][badgeData.distinction];
-				console.log("Distinction", backgroundImgPath);
-				let sportImgPath = BadgeImageConfig['sport']['v4'][badgeData['sport']][(new Date(badgeData['awardDate'])).getFullYear() % 2];
-				console.log("Sport", sportImgPath);
+				
+				/* Get the correct badge sport layer
+					Switch between blue and white icons in alternating years
+					If the distinction is 1 (Gold) use the gold sports image
+				*/
 
-				// let renderEngineV2 = new RenderEngineV2(canvas, [
-				// 	{
-				// 		type: "image",
-				// 		name: "background",
-				// 		imagePath: backgroundImgPath,
-				// 		x: 125,
-				// 		y: 0
-				// 	},
-				// 	{
-				// 		type: "image",
-				// 		name: "sport",
-				// 		imagePath: sportImgPath,
-				// 		x: 325,
-				// 		y: 455
-				// 	},
-				// 	{
-				// 		type: "text",
-				// 		name: "badge name",
-				// 		fontSize: "45px",
-				// 		fontFamily: "Helvetica",
-				// 		fontColor: "white",
-				// 		x: 380,
-				// 		y: 435,
-				// 		text: badgeData.awardName
-				// 	},
-				// 	{
-				// 		type: "text",
-				// 		name: "recpient name",
-				// 		fontSize: "28px",
-				// 		fontColor: "black",
-				// 		fontFamily: "Helvetica",
-				// 		x: 380,
-				// 		y: 160,
-				// 		text: badgeData.recipientName
-				// 	}
-				// ] )
-				// renderEngineV2.setScaleFactor(1.8, 1.8);
-				// renderEngineV2.drawLayersMethodConditional()
+				let sportLayerSelectorValue;
+				if (badgeData['distinction'] == 1) { // use gold sport badge
+					sportLayerSelectorValue = "gold";
+				} else { // determine whether it is an even or odd year
+					sportLayerSelectorValue = (new Date(badgeData['awardDate'])).getFullYear() % 2;
+				}
+				
+				let sportImgPath = BadgeImageConfig['sport']['v4'][badgeData['sport']][sportLayerSelectorValue];
+				
 
+				// Render the badge
 				let renderEngine = new RenderEngine(canvas, [
 					{
 						type: "image",
 						name: "background",
 						imagePath: backgroundImgPath,
-						x: 125,
+						x: 175,
 						y: 0
 					},
 					{
 						type: "image",
 						name: "sport",
 						imagePath: sportImgPath,
-						x: 330,
+						x: 380,
 						y: 600
 					},
 					{
@@ -260,7 +204,7 @@ $(() => {
 						fontSize: "50px",
 						fontFamily: "Helvetica",
 						fontColor: "white",
-						x: 400,
+						x: 450,
 						y: 565,
 						text: badgeData.awardName
 					},
@@ -270,14 +214,14 @@ $(() => {
 						fontSize: "28px",
 						fontColor: "black",
 						fontFamily: "Helvetica",
-						x: 410,
+						x: 460,
 						y: 220,
 						text: badgeData.recipientName
 					}
 				] )
-				//renderEngine.setScaleFactor(1.8, 1.8);
+				
 				renderEngine.drawImage()
-				.then((output) => {
+				.then((output) => { // Create a image url of the badge image to store
 					let imageURI = canvas.toDataURL("imageURI/png");
 					badgeData.imageURI = imageURI;
 					let myImage = new Image();
@@ -285,12 +229,15 @@ $(() => {
 					return myImage
 				})
 				.then((myImage) => {
-					// The data URL stored inside the badge object in local storage is correct but the one used as an image source is incorrect		
+					// Store the badge image url along with badge data in local storage	
 					window.localStorage.setItem('Badge' + hash, JSON.stringify(badgeData));
 					let badgeMapKey = "Badge-" + Object.keys(mapOfBadges).length
 					mapOfBadges[badgeMapKey] = badgeData;
-					console.log(mapOfBadges);
+
+					// Remove canvas used to draw the badge
 					$(canvasId).remove();
+
+					// Generate badge card to display newly unlocked badge
 					let cardId = badgeData.badgeName+badgeData.recipientName;
 					$("#badges").append(`<a href="#badgeStage"><div class="badgeCard" data-badge="${badgeMapKey}" id="${cardId}-Card">
 						<div class="badgeImgContainer" id="${cardId}" >
@@ -301,15 +248,17 @@ $(() => {
 			   		 </div></a>`);
 					document.getElementById(cardId).appendChild(myImage);
 					let cardSelectorString = "#" + cardId + "-Card";
+
+					// Attach an event handler that allows the badge to be put in focus if selected
 					$(cardSelectorString).on("click", function(event) {
-						console.log(this);
-						//Set Badge Image to Badge Stage
-						//let imageURI = $(this).children(".badgeImgContainer").children("img").attr("src");
-						
+						//Set Badge Image to the top center of the sceen (enlarged)
 						$("#badge").attr("src", badgeData.imageURI);
 						$("#badge").attr("data-badge", badgeMapKey);
 						$("#badgeNameDisplay").text(badgeData.badgeName);
 					});
+
+					// Hide unlock badge components like the input form
+					// Display the unlocked badge
 					$("#badgeUnlockStatus").text("Congratulations! Your has been unlocked!");
 					$("#badgeDisplayed").show();
 					$("#needsToBeUnlocked").hide();
@@ -318,62 +267,51 @@ $(() => {
 					$("#badgeNameDisplay").text(badgeData.badgeName);
 					drawCertificate1(badgeData)
 				});	
-			} catch(err) {
-				console.log(err);
+			} catch(err) { // Wrong key was entered
 				$("#badgeUnlockStatus").text("");
 				feedback.text("Wrong key... Please try again");
 			}
-		} else {
+		} else { // You already have the badge
 			feedback.text("You already have this badge")
 			$("#badgeUnlockStatus").text("");
 		}
 	});
-
-	console.log(getBadgesFromLocalStorage());
 	
 	
-	// When a badge card is clicked load that badge onto the badge stage
-	// $(".badgeCard").on('click', function(evt) {
-	// 	// Get Badge Image Src
-	// 	console.log(this);
-	// 	//const badgeURI = $()
-	// })
+	// Draw the certificate based on badge data
 	function drawCertificate1(badgeData) {
-		let imageURI = badgeData.imageURI;
-		console.log(imageURI);
+		// Get reference to certicate drawing canvas
 		let certificateCanvas = document.getElementById("myCertificate");
 		// Clear Canvas
 		let context = certificateCanvas.getContext("2d");
+
+		// Reset the canvas by drawing a blank layer over everything
 		context.clearRect(0, 0, certificateCanvas.width, certificateCanvas.height);
-		//certificateCanvas.style.display = "block";
+		
+		// Draw the certificate
 		let certRender = new RenderEngine(certificateCanvas, [
 				{
 					type: "image",
 					name: "certBase",
-					imagePath: './CertificateAssets/Certificate_Blank.PNG',
+					imagePath: './CertificateAssets/Certificate-template.png',
 					x: 0,
 					y: 0
 				},
-				// {
-				// 	type: "image",
-				// 	name: "badge",
-				// 	imagePath: imageURI,
-				// 	x: 100,
-				// 	y: 100
-				// },
 				{
 					type: "image",
 					name: "sport",
-					scalex: 2.5,
-					scaley: 2.5,
+					// scalex: 2.5,
+					// scaley: 2.5,
+
 					x: 130,
 					y: 140,
-					imagePath: BadgeImageConfig['sport'][badgeData.sport][1]
+					imagePath: BadgeImageConfig['sport']['v4'][badgeData.sport][1]
 				},
 				{
 					type: "text",
 					name: "date",
 					fontSize: "30px",
+					fontColor: "#1f0f66",
 					fontFamily: "SignatureScript",
 					x: 540,//440,
 					y: 720, // 690,
@@ -381,43 +319,44 @@ $(() => {
 				},
 				{
 					type: "text",
+					name: "issuer",
+					fontSize: "30px",
+					fontColor: "#1f0f66",
+					fontFamily: "SignatureScript",
+					x: 850,//440,
+					y: 720, // 690,
+					text: badgeData.issuerName
+				},
+				{
+					type: "text",
 					name: "recipientName",
-					fontSize: "54px",
-					fontFamily: "RobotoLight",
+					fontSize: "48px",
+					fontColor: "#1f0f66",
+					fontFamily: "Arial",
 					x: 675,//440,
-					y: 320, // 690,
+					y: 350, // 690,
 					text: badgeData.recipientName
 				},
 				{
 					type: "text",
-					name: "title",
-					fontSize: "45px",
-					fontFamily: "SignatureScript",
-					x: 500,
-					y: 50,
-					text: `${badgeData.badgeName} Awarded To ${badgeData.recipientName}`
-				},
-				{
-					type: "text",
-					name: "issuer",
-					fontSize: "30px",
-					fontFamily: "Serif",
-					x: 600,
-					y: 700,
-					text: `Awarded by ${badgeData.issuerName}, Director of Sports Administration on ${badgeData.awardDate}.`
+					name: "awardName",
+					fontSize: "54px",
+					fontColor: "#1f0f66",
+					fontFamily: "Helvetica",
+					x: 675,//440,
+					y: 450, // 690,
+					text: badgeData.awardName
 				}
 			]);
-		certRender.setScaleFactor(1.11510031679, 1.11627906977);
 		certRender.drawImage();
 	}
 
+	// Handles the download of the certificate
 	let download = document.getElementById("pdfBtn");
 		download.addEventListener("click", function() {
 			let certCanvas = document.getElementById("myCertificate")
 			var imgData = certCanvas.toDataURL("image/png");
-			//certCanvas.style.display = "block";
 	  		var pdf = new jsPDF('l');
-	  		//pdf.addImage(imgData, 'JPEG', 15, 40, 180, 160)
 	  		pdf.addImage(imgData, 'JPEG', 0, 0);
 	  		pdf.save("download.pdf");
 	  	}, false);	
