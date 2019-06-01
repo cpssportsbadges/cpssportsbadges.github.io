fccce2399334a46676275bf58f815413cc22fa46
Joseph Varilla <jvarilla981@gmail.com>
2019-05-31 21:31:06 -0500

    Added comments for the date arrays

--- BadgeRenderer/main.js
+++ BadgeRenderer/main.js
@@ -20,13 +20,15 @@ const RenderEngine = require("./RenderEngine.js");
 let jsPDF = require("jspdf");
 let decryptedText;
 let ciphertext;
+
+// Dates and months used for certificate generation
 const datesWithSuffixes = ['0th', '1st', '2nd', '3rd', 
 	'4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th',
 	'12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th',
 	'20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th',
 	'28th', '29th', '30th', '31st'];
 
-	const monthNames = ['January', 'February', 'March', 'April',
+const monthNames = ['January', 'February', 'March', 'April',
 	'May', 'June', 'July', 'August', 'September', 'October', 'November',
 	'December'];
 
